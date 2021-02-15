c973f8ab43252f88f911ce76002418f95ea50ef0
b-bella99 <bella.setyowati99@gmail.com>
2021-02-15 10:40:32 +0700

    Upload source code modul 1

---
+++ minggu1/class.js
@@ -0,0 +1,28 @@
+// Super class
+
+class Holiday {
+    constructor(destination, days){
+        this.destination = destination;
+        this.days = days;
+    }
+
+    info(){
+        alert(`${this.destination} will take ${this.days} days.`);
+    }
+}
+
+// Sub class
+class Expedition extends Holiday {
+    constructor(destination, days, gear) {
+        super(destination, days);
+        this.gear = gear;
+    }
+
+    info(){
+        super.info();
+        alert(`Bring your ${this.gear.join(' and your ')}`);
+    }
+}
+
+const tripWithGear = new Expedition('Semeru', 10, ['Sunglasess', 'Flags', 'Camera']);
+tripWithGear.info();
