a6f2d6726097e595550bb6af7575511aaeb41cf9
b-bella99 <bella.setyowati99@gmail.com>
2021-04-14 17:11:22 +0700

    Upload SC practicum - not done yet

---
+++ minggu10/react-firebase/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyB2PyZNr9bwzvd4hWsmT4VztrW6_nmZSUE",
+    authDomain: "react-firebase-56faa.firebaseapp.com",
+    projectId: "react-firebase-56faa",
+    storageBucket: "react-firebase-56faa.appspot.com",
+    messagingSenderId: "660458951802",
+    appId: "1:660458951802:web:0934259d8f281d057657c3",
+    measurementId: "G-3JJ85LXGQE"
+};
