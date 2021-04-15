b2ce35c7338dbbe18266b92a60e6c9aeb0893af7
b-bella99 <bella.setyowati99@gmail.com>
2021-04-15 20:34:05 +0700

    Modify Header - add return

--- minggu10/react-firebase/src/Header.js
+++ minggu10/react-firebase/src/Header.js
@@ -3,13 +3,15 @@ import routes from "./routes";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-    <ul className = "nav">
+    return(
+        <ul className = "nav">
         {routes.map((route, i) => (
             <li key = {i}>
                 <Link to = {route.path}>{route.name}</Link>
             </li>
         ))}
     </ul>
+    );
 };
 
 export default Header;
