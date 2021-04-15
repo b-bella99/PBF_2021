0353320eef93b22ac69cb076d4a96883a32ca0c4
b-bella99 <bella.setyowati99@gmail.com>
2021-04-15 20:34:38 +0700

    Modify Joim - practicum done

--- minggu10/react-firebase/src/Join.js
+++ minggu10/react-firebase/src/Join.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext } from "react";
 import { AuthContext } from "./index";
+import firebase from "firebase";
 
 const Join = () => {
     const [email, setEmail] = useState("");
@@ -9,8 +10,13 @@ const Join = () => {
     const Auth = useContext(AuthContext);
     const handleForm = e => {
         e.preventDefault();
-        console.log(Auth);
-        Auth.setLoggedIn(true);
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(res => {
+            if (res.user) Auth.setLoggedIn(true);
+        })
+        .catch(e => {
+            setError(e.message);
+        });
     };
 
     return (
@@ -40,7 +46,7 @@ const Join = () => {
                     />
                     Join With Google
                 </button>
-                <button type="submit">Login</button>
+                <button type="submit">Join</button>
                 <span>{error}</span>
             </form>
         </div>
